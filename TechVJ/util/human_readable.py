79df732843814317db1dd9dbce5b018f7d202f41
mntcloudop <mntcloudX@gmail.com>
2025-11-09 15:15:05 +0530

    Update human_readable.py

--- TechVJ/util/human_readable.py
+++ TechVJ/util/human_readable.py
@@ -1,12 +1,27 @@
+# def humanbytes(size):
+#     # https://stackoverflow.com/a/49361727/4723940
+#     # 2**10 = 1024
+#     if not size:
+#         return ""
+#     power = 2**10
+#     n = 0
+#     Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+#     while size > power:
+#         size /= power
+#         n += 1
+#     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
 def humanbytes(size):
-    # https://stackoverflow.com/a/49361727/4723940
-    # 2**10 = 1024
+    """
+    Convert bytes to human-readable format (decimal base).
+    Example: 1024*1024 -> 1.05 MB
+    """
     if not size:
         return ""
-    power = 2**10
+    power = 1000  # decimal
     n = 0
-    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
-    while size > power:
+    units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+    while size >= power and n < len(units) - 1:
         size /= power
         n += 1
-    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+    return f"{size:.2f} {units[n]}"
